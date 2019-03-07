8fd8369e5dc9f18625af60d6c6009116fda0575e
Audrey Ford <ariding@hawaii.edu>
2019-03-06 20:58:29 -1000

    Finished branch

--- app/server/main.js
+++ app/server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
-import './PeopleInitialization';
+import { people } from './PeopleInitialization';
 
 Meteor.startup(() => {
   // code to run on server at startup
